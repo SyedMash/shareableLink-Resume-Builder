91e99d441459836f75aadbdac89af256e2811b85
Syed Mashhood Hussain <mashrtx7@gmail.com>
2024-09-13 15:56:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+var subtn = document.querySelector("#subtn");
+var form = document.querySelector("form");
+var result = document.querySelector("#result");
+var editbtn = document.querySelector("#editBtn");
+// data from form
+var namme = document.querySelector("#name");
+var email = document.querySelector("#email");
+var number = document.querySelector("#number");
+var edu = document.querySelector("#edu");
+var work = document.querySelector("#work");
+var skills = document.querySelector("#skills");
+// data to show
+var showname = document.querySelector("#showname");
+var showemail = document.querySelector("#showemail");
+var shownumber = document.querySelector("#shownumber");
+var showedu = document.querySelector("#showedu");
+var showork = document.querySelector("#showork");
+var showskills = document.querySelector("#showskills");
+var shareLinkCont = document.querySelector("#shareAbleLinkCont");
+var shareLinkAnchor = document.querySelector("#getLink");
+var downloadPdf = document.querySelector("#download");
+//click event on generate button
+subtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (namme.value !== "" &&
+        email.value !== "" &&
+        number.value !== "" &&
+        edu.value !== "" &&
+        work.value !== "" &&
+        skills.value !== "") {
+        form.style.display = "none";
+        result.style.display = "flex";
+        editbtn.style.display = "block";
+        showname.innerHTML = "Name: ".concat(namme.value);
+        showemail.innerHTML = "Email: ".concat(email.value);
+        shownumber.innerHTML = "Number: ".concat(number.value);
+        showedu.innerHTML = "Education: ".concat(edu.value);
+        showork.innerHTML = "Work Experience: ".concat(work.value);
+        skills.value.split(",").forEach(function (e) {
+            var skillElement = document.createElement("h3");
+            skillElement.classList.add("setSkills");
+            skillElement.textContent = e.trim();
+            showskills.appendChild(skillElement);
+        });
+        var queryString = new URLSearchParams({
+            username: namme.value,
+            email: email.value,
+            number: number.value,
+            edu: edu.value,
+            work: work.value,
+            skills: skills.value,
+        });
+        var link = "".concat(window.location.origin, "?").concat(queryString);
+        shareLinkAnchor.href = link;
+        shareLinkCont.style.display = "flex";
+        shareLinkAnchor.textContent = "Your Link";
+    }
+});
+window.addEventListener("DOMContentLoaded", function () {
+    var param = new URLSearchParams(window.location.search);
+    if (param.has("username")) {
+        (form.style.display = "none"), (result.style.display = "flex");
+    }
+    var username = param.get("username") || "";
+    var emailVal = param.get("email") || "";
+    var numberVal = param.get("number") || "";
+    var eduVal = param.get("edu") || "";
+    var workVal = param.get("work") || "";
+    var skillsVal = param.get("skills") || "";
+    showname.innerHTML = "Name: ".concat(username);
+    showemail.innerHTML = "Email: ".concat(emailVal);
+    shownumber.innerHTML = "Number: ".concat(numberVal);
+    showedu.innerHTML = "Education: ".concat(eduVal);
+    showork.innerHTML = "Work Experience: ".concat(workVal);
+    if (skillsVal.trim() !== "") {
+        skillsVal.split(",").forEach(function (e) {
+            var skillElement = document.createElement("h3");
+            skillElement.classList.add("setSkills");
+            skillElement.textContent = e.trim();
+            showskills.appendChild(skillElement);
+        });
+    }
+});
+//edit button event listener to edit the values
+editbtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    form.style.display = "flex";
+    result.style.display = "none";
+    editbtn.style.display = "none";
+    shareLinkCont.style.display = "none";
+    while (showskills.firstChild) {
+        showskills.removeChild(showskills.firstChild);
+    }
+});
+//download as pdf function
+downloadPdf.addEventListener("click", function () {
+    shareLinkCont.style.display = "none";
+    subtn.style.display = "none";
+    window.print();
+});
---
+++ script.ts
@@ -0,0 +1,117 @@
+const subtn = document.querySelector("#subtn") as HTMLButtonElement;
+const form = document.querySelector("form") as HTMLFormElement;
+const result = document.querySelector("#result") as HTMLButtonElement;
+const editbtn = document.querySelector("#editBtn") as HTMLButtonElement;
+
+// data from form
+const namme = document.querySelector("#name") as HTMLInputElement;
+const email = document.querySelector("#email") as HTMLInputElement;
+const number = document.querySelector("#number") as HTMLInputElement;
+const edu = document.querySelector("#edu") as HTMLInputElement;
+const work = document.querySelector("#work") as HTMLInputElement;
+const skills = document.querySelector("#skills") as HTMLInputElement;
+
+// data to show
+const showname = document.querySelector("#showname") as HTMLHeadingElement;
+const showemail = document.querySelector("#showemail") as HTMLHeadingElement;
+const shownumber = document.querySelector("#shownumber") as HTMLHeadingElement;
+const showedu = document.querySelector("#showedu") as HTMLHeadingElement;
+const showork = document.querySelector("#showork") as HTMLHeadingElement;
+const showskills = document.querySelector("#showskills") as HTMLDivElement;
+const shareLinkCont = document.querySelector(
+  "#shareAbleLinkCont"
+) as HTMLDivElement;
+const shareLinkAnchor = document.querySelector("#getLink") as HTMLAnchorElement;
+const downloadPdf = document.querySelector("#download") as HTMLButtonElement
+
+//click event on generate button
+subtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    namme.value !== "" &&
+    email.value !== "" &&
+    number.value !== "" &&
+    edu.value !== "" &&
+    work.value !== "" &&
+    skills.value !== ""
+  ) {
+    form.style.display = "none";
+    result.style.display = "flex";
+    editbtn.style.display = "block";
+
+    showname.innerHTML = `Name: ${namme.value}`;
+    showemail.innerHTML = `Email: ${email.value}`;
+    shownumber.innerHTML = `Number: ${number.value}`;
+    showedu.innerHTML = `Education: ${edu.value}`;
+    showork.innerHTML = `Work Experience: ${work.value}`;
+        skills.value.split(",").forEach((e) => {
+            const skillElement = document.createElement("h3");
+            skillElement.classList.add("setSkills");
+            skillElement.textContent = e.trim();
+            showskills.appendChild(skillElement);
+        });
+
+    const queryString = new URLSearchParams({
+      username: namme.value,
+      email: email.value,
+      number: number.value,
+      edu: edu.value,
+      work: work.value,
+      skills: skills.value,
+    });
+
+    const link = `${window.location.origin}?${queryString}`;
+    shareLinkAnchor.href = link;
+    shareLinkCont.style.display = "flex";
+    shareLinkAnchor.textContent = "Your Link";
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const param = new URLSearchParams(window.location.search);
+  if (param.has("username")) {
+    (form.style.display = "none"), (result.style.display = "flex");
+  }
+  const username = param.get("username") || "";
+  const emailVal = param.get("email") || "";
+  const numberVal = param.get("number") || "";
+  const eduVal = param.get("edu") || "";
+  const workVal = param.get("work") || "";
+  const skillsVal = param.get("skills") || "";
+
+  showname.innerHTML = `Name: ${username}`;
+  showemail.innerHTML = `Email: ${emailVal}`;
+  shownumber.innerHTML = `Number: ${numberVal}`;
+  showedu.innerHTML = `Education: ${eduVal}`;
+  showork.innerHTML = `Work Experience: ${workVal}`;
+
+  if(skillsVal.trim() !== ""){
+      skillsVal.split(",").forEach((e) => {
+          const skillElement = document.createElement("h3");
+          skillElement.classList.add("setSkills");
+          skillElement.textContent = e.trim();
+          showskills.appendChild(skillElement);
+        });
+    }
+});
+
+//edit button event listener to edit the values
+editbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  form.style.display = "flex";
+  result.style.display = "none";
+  editbtn.style.display = "none";
+  shareLinkCont.style.display = "none";
+
+  while (showskills.firstChild) {
+    showskills.removeChild(showskills.firstChild);
+  }
+});
+
+
+//download as pdf function
+downloadPdf.addEventListener("click", () => {
+  shareLinkCont.style.display = "none"
+  subtn.style.display = "none"
+  window.print()
+})
